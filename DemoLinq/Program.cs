f3633e668a10582bca756c9a30001100f326cd2f
Quentin Geerts <geerts.quentin@gmail.com>
2026-01-07 16:25:35 +0100

    Démonstration 05 - Linq (Partie 1)

--- DemoLinq/Program.cs
+++ DemoLinq/Program.cs
@@ -7,7 +7,9 @@ using DemoLinq.Models;
 List<Contact> Contacts = new List<Contact>();
 Contacts.AddRange(new Contact[] {
     new Contact(){ Nom = "Person", Prenom="Michael", Email="michael.person@cognitic.be", AnneeDeNaissance = 1982 },
+    new Contact(){ Nom = "Person", Prenom="Michael", Email="michael.person2@cognitic.be", AnneeDeNaissance = 1982 },
     new Contact(){ Nom = "Morre", Prenom="Thierry", Email="thierry.morre@cognitic.be", AnneeDeNaissance = 1974 },
+    new Contact(){ Nom = "Morre", Prenom="Thierry", Email="thierry.morre2@cognitic.be", AnneeDeNaissance = 1974 },
     new Contact(){ Nom = "Dupuis", Prenom="Thierry", Email="thierry.dupuis@cognitic.be", AnneeDeNaissance = 1988 },
     new Contact(){ Nom = "Faulkner", Prenom="Stéphane", Email="stephane.faulkner@cognitic.be", AnneeDeNaissance = 1969 },
     new Contact(){ Nom = "Selleck", Prenom = "Tom", Email = "tom.selleck@imdb.com", AnneeDeNaissance = 1945 },
@@ -102,7 +104,82 @@ foreach (var c in contactsModifies)
     Console.WriteLine($"Contact: {c.NomComplet} {c.Courriel}");
 }
 
-foreach (var c in contactsModifies2)
+
+// 5.  Opérateur Distinct
+// Permet de supprimer les doublons d'une séquence
+
+var contactsSansDoublon = Contacts.Select(c => new { c.Nom, Prenom = c.Prenom }).Distinct(); // Opérateur
+var contactsSansDoublon2 = (from Contact c in Contacts 
+                           select new { c.Nom, Prenom = c.Prenom }).Distinct(); // Expression de requête
+
+foreach (var c in contactsSansDoublon)
 {
-    Console.WriteLine($"Contact: {c.NomComplet} {c.Courriel}");
+    Console.WriteLine($"Contact sans doublon: {c.Nom} {c.Prenom}");
+}
+
+foreach (var c in contactsSansDoublon2)
+{
+    Console.WriteLine($"Contact sans doublon: {c.Nom} {c.Prenom}");
+}
+
+
+// 6.  Opérateur SingleOrDefault
+// Permet de retourner un élément uniquement d'une séquence ou une valeur par défaut si aucun élément ne correspond à la condition
+// <!> SingleOrDefault() peut déclencher une exception quand plus d'une valeur !!
+
+Contact? thierryMorre = Contacts
+    .Where(c => c.Nom == "Morre" && c.Prenom == "Thierry")
+    .DistinctBy(c => new { c.Nom, c.Prenom }) // Solution pour SingleOrDefault dans le cas de doublons
+    .SingleOrDefault(); // Opérateur
+
+Contact? stephaneFaulkner = Contacts.SingleOrDefault(c => c.Nom == "Faulkner" && c.Prenom == "Stéphane"); // Opérateur
+
+
+Contact? quentinGeerts = (from Contact c in Contacts
+                         where c.Nom == "Geerts" && c.Prenom == "Quentin"
+                         select c).SingleOrDefault() ?? new Contact { Nom = "Geerts", Prenom = "Quentin" }; // Expression de requête
+
+
+// 7.  Opérateur FirstOrDefault
+// Permet de retourner le premier élément d'une séquence ou une valeur par défaut si aucun élément ne correspond à la condition
+// Ne déclenchera pas d'erreur dans le cas où plus d'une valeur
+
+Contact? michaelPerson = Contacts
+    .Where(c => c.Nom == "Person" && c.Prenom == "Michael")
+    .FirstOrDefault();
+
+Contact? michaelPerson2 = Contacts.FirstOrDefault(c => c.Nom == "Person" && c.Prenom == "Michael");
+
+
+// 8.  Opérateur OrderBy | OrderByDescending
+// Permet de trier les éléments d'une séquence en ordre alphabétique sur base d'un champ
+
+var contactsTriesParAnnee = Contacts.OrderBy(c => c.AnneeDeNaissance); // Opérateur
+var contactsTriesParAnnee2 = from Contact c in Contacts orderby c.AnneeDeNaissance select c; // Expression de requête
+
+var contactsTriesParAnneeInv = Contacts.OrderByDescending(c => c.AnneeDeNaissance); // Opérateur
+var contactsTriesParAnneeInv2 = from Contact c in Contacts orderby c.AnneeDeNaissance descending select c; // Expression de requête
+
+foreach (var c in contactsTriesParAnnee)
+{
+    Console.WriteLine($"Contact trié: {c.Nom} {c.Prenom} {c.AnneeDeNaissance}");
+}
+
+foreach (var c in contactsTriesParAnneeInv)
+{
+    Console.WriteLine($"Contact trié: {c.Nom} {c.Prenom} {c.AnneeDeNaissance}");
+}
+
+
+// 9.  Opérateurs ThenBy | ThenByDescending
+// Permet d'ajouter un tri secondaire sur les éléments d'une séquence
+
+var contactsAvecDeuxTris = Contacts.OrderBy(c => c.AnneeDeNaissance).ThenBy(c => c.Nom);
+var contactsAvecDeuxTris2 = from Contact c in Contacts 
+                            orderby c.AnneeDeNaissance, c.Nom descending 
+                            select c; // Expression de requête
+
+foreach (var c in contactsAvecDeuxTris)
+{
+    Console.WriteLine($"2 tri: {c.AnneeDeNaissance} {c.Nom} {c.Prenom}");
 }
