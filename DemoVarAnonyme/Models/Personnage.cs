7c6898edbc2deee2b769c19a44d175b14bbd9f1e
Quentin Geerts <geerts.quentin@gmail.com>
2026-01-07 09:51:46 +0100

    Add project files.

---
+++ DemoVarAnonyme/Models/Personnage.cs
@@ -0,0 +1,18 @@
+﻿namespace DemoVarAnonyme.Models;
+
+public enum TypePersonnage
+{
+    Humain,
+    Ane,
+    Dragon,
+    Grenouille,
+    Ogre
+}
+
+public class Personnage
+{
+    public string Nom { get; set; }
+    public TypePersonnage Type { get; set; }
+    public string CouleurPeau { get; set; }
+}
+
---
+++ DemoVarAnonyme/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *  Démonstration 01 - Var et types anonymes
+ */
+
+
+using DemoVarAnonyme.Models;
+
+List<Personnage> personnagesShrek =
+[
+    new Personnage { Nom = "Shrek", Type = TypePersonnage.Ogre, CouleurPeau = "Vert" },
+    new Personnage { Nom = "L'Âne", Type = TypePersonnage.Ane, CouleurPeau = "Gris" },
+    new Personnage { Nom = "Fiona", Type = TypePersonnage.Ogre, CouleurPeau = "Vert" },
+    new Personnage { Nom = "Lord Farquaad", Type = TypePersonnage.Humain, CouleurPeau = "Beige" },
+];
+
+// Parcourt de la liste et récupération des données modifiées
+foreach (Personnage p in personnagesShrek)
+{
+    // Création d'un type anonyme
+    var personnage = new { Nom = p.Nom, Race = p.Type };
+
+    Console.WriteLine($"Personnage: {personnage.Nom} {personnage.Race}");
+}
