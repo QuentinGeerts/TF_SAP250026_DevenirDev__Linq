9240ba809eb50827dcd4f7b784abf5801e1c653c
Quentin Geerts <geerts.quentin@gmail.com>
2026-01-08 14:47:00 +0100

    Correction exercices - Partie 1 (terminé)

--- Exo_Linq_App/Program.cs
+++ Exo_Linq_App/Program.cs
@@ -110,4 +110,138 @@ foreach (var s in resultat2_1a)
     Console.WriteLine($"{s.LastName} {s.YearResult} {s.Statut}");
 }
 
+// Exercice 2.2 Donner pour chaque étudiant entre 1955 et 1965 le nom, le résultat annuel et la
+// catégorie à laquelle il appartient. La catégorie est fonction du résultat annuel obtenu ; un
+// résultat inférieur à 10 appartient à la catégorie « inférieure », un résultat égal à 10 appartient
+// à la catégorie « neutre », un résultat autre appartient à la catégorie « supérieure ».
+
+Console.WriteLine($"\nExercice 2.2.\n");
+
+var resultat2_2a = context.Students
+    .Where(s => s.BirthDate.Year >= 1955 && s.BirthDate.Year <= 1965)
+    .Select(s => new
+    {
+        LastName = s.Last_Name,
+        YearResult = s.Year_Result,
+        Categorie = s.Year_Result < 10 ? "inférieure" : s.Year_Result == 10 ? "neutre" : "supérieure"
+    });
+
+var resultat2_2b = from s in context.Students
+                   where s.BirthDate.Year >= 1955 && s.BirthDate.Year <= 1965
+                   select new
+                   {
+                       LastName = s.Last_Name,
+                       YearResult = s.Year_Result,
+                       Categorie = s.Year_Result < 10 ? "inférieure" : s.Year_Result == 10 ? "neutre" : "supérieure"
+                   };
+
+foreach (var s in resultat2_2a)
+{
+    Console.WriteLine($"{s.LastName} {s.YearResult} {s.Categorie}");
+}
+
+// Exercice 2.3 Ecrire une requête pour présenter le nom, l’id de section et de tous les étudiants
+// qui ont un nom de famille qui termine par r.
+
+Console.WriteLine($"\nExercice 2.3.\n");
+
+var resultat2_3a = context.Students
+    .Where(s => s.Last_Name.EndsWith("r"))
+    .Select(s => new
+    {
+        LastName = s.Last_Name,
+        SectionId = s.Section_ID
+    });
+
+foreach (var s in resultat2_3a)
+{
+    Console.WriteLine($"{s.LastName} {s.SectionId}");
+}
+
+
+// Exercice 2.4 Ecrire une requête pour présenter le nom et le résultat annuel classé par résultats
+// annuels décroissant de tous les étudiants qui ont obtenu un résultat annuel inférieur ou égal
+// à 3.
+
+Console.WriteLine($"\nExercice 2.4.\n");
+
+var resultat2_4 = context.Students
+    .Where(s => s.Year_Result <= 3)
+    .Select(s => new
+    {
+        LastName = s.Last_Name,
+        YearResult = s.Year_Result
+    })
+    .OrderByDescending(s => s.YearResult);
+
+foreach (var s in resultat2_4)
+{
+    Console.WriteLine($"{s.LastName} {s.YearResult}");
+}
+
+
+// Exercice 2.5 Ecrire une requête pour présenter le nom complet (nom et prénom séparés par un
+// espace) et le résultat annuel classé par nom croissant sur le nom de tous les étudiants
+// appartenant à la section 1110.
+
+Console.WriteLine($"\nExercice 2.5.\n");
+
+var resultat2_5 = context.Students
+    .Where(s => s.Section_ID == 1110)
+    .Select(s => new
+    {
+        NomComplet = $"{s.Last_Name} {s.First_Name}",
+        YearResult = s.Year_Result
+    })
+    .OrderBy(s => s.NomComplet);
+
+foreach (var s in resultat2_5)
+{
+    Console.WriteLine($"{s.NomComplet} {s.YearResult}");
+}
+
+// Exercice 2.6 Ecrire une requête pour présenter le nom, l’id de section et le résultat annuel
+// classé par ordre croissant sur la section de tous les étudiants appartenant aux sections 1010
+// et 1020 ayant un résultat annuel qui n’est pas compris entre 12 et 18.
+
+Console.WriteLine($"\nExercice 2.6.\n");
+
+var resultat2_6 = context.Students
+    .Where(s => (s.Section_ID == 1010 || s.Section_ID == 1020) && !(s.Year_Result >= 12 && s.Year_Result <= 18))
+    .Select(s => new
+    {
+        LastName = s.Last_Name,
+        SectionId = s.Section_ID,
+        YearResult = s.Year_Result
+    })
+    .OrderBy(s => s.SectionId)
+    .ThenByDescending(s => s.YearResult);
+
+foreach (var s in resultat2_6)
+{
+    Console.WriteLine($"{s.SectionId} {s.YearResult} {s.LastName}");
+}
+
+// Exercice 2.7 Ecrire une requête pour présenter le nom, l’id de section et le résultat annuel sur
+// 100 (nommer la colonne ‘result_100’) classé par ordre décroissant du résultat de tous les
+// étudiants appartenant aux sections commençant par 13 et ayant un résultat annuel sur 100
+// inférieur ou égal à 60.
+
+Console.WriteLine($"\nExercice 2.7.\n");
+
+var resultat2_7 = context.Students
+    .Where(s => (s.Section_ID.ToString().StartsWith("13")) && (s.Year_Result * 5 <= 60))
+    .Select(s => new
+    {
+        LastName = s.Last_Name,
+        SectionId = s.Section_ID,
+        Resultat100 = s.Year_Result * 5
+    })
+    .OrderByDescending(s => s.Resultat100);
+
+foreach (var s in resultat2_7)
+{
+    Console.WriteLine($"{s.LastName} {s.SectionId} {s.Resultat100}");
+}
+
 
