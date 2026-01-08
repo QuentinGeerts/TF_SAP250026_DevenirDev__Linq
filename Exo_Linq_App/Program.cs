d8547d6c5d1e6a0475d9ea54c9575cb277be8515
Quentin Geerts <geerts.quentin@gmail.com>
2026-01-08 11:38:30 +0100

    Correction exercice 1.2 et 1.3

--- Exo_Linq_App/Program.cs
+++ Exo_Linq_App/Program.cs
@@ -11,6 +11,8 @@ Console.WriteLine($"1.  Opérateur « Select »");
 // Exercice 1.1 Ecrire une requête pour présenter, pour chaque étudiant, le nom de l’étudiant, la
 // date de naissance, le login et le résultat pour l’année de l’ensemble des étudiants.
 
+Console.WriteLine($"\nExercice 1.1.\n");
+
 var resultat1_1a = context.Students.Select(s => new
 {
     LastName = s.Last_Name,
@@ -34,19 +36,45 @@ foreach (var s in resultat1_1a)
     Console.WriteLine($"{s.LastName} {s.BirthDate} {s.Login} {s.YearResult}");
 }
 
-Console.WriteLine();
-
-foreach (var s in resultat1_1b)
-{
-    Console.WriteLine($"{s.LastName} {s.BirthDate} {s.Login} {s.YearResult}");
-}
 
 // Exercice 1.2 Ecrire une requête pour présenter, pour chaque étudiant, son nom complet (nom
 // et prénom séparés par un espace), son id et sa date de naissance.
 
+Console.WriteLine($"\nExercice 1.2.\n");
 
+
+var resultat1_2a = context.Students
+    .Select(s => new { 
+        NomComplet = $"{s.Last_Name} {s.First_Name}", 
+        Id = s.Student_ID,
+        s.BirthDate
+    });
+
+var resultat1_2b = from s in context.Students
+                   select new
+                   {
+                       NomComplet = $"{s.Last_Name} {s.First_Name}",
+                       Id = s.Student_ID,
+                       s.BirthDate
+                   };
+
+foreach (var s in resultat1_2a)
+{
+    Console.WriteLine($"{s.Id} {s.NomComplet} {s.BirthDate}");
+}
 
 // Exercice 1.3 Ecrire une requête pour présenter, pour chaque étudiant, dans une seule chaine de
 // caractère l’ensemble des données relatives à un étudiant séparées par le symbole |.
 
+Console.WriteLine($"\nExercice 1.3.\n");
 
+var resultat1_3a = context.Students
+    .Select(s => $"{s.Student_ID}|{s.Last_Name}|{s.First_Name}|{s.BirthDate}|{s.Login}|{s.Year_Result}|{s.Course_ID}|{s.Section_ID}");
+
+var resultat1_3b = from s in context.Students 
+                   select string.Join("|", s.Student_ID, s.Last_Name, s.First_Name, s.BirthDate, s.Login, s.Year_Result, s.Course_ID, s.Section_ID);
+
+foreach (var s in resultat1_3a)
+{
+    Console.WriteLine($"{s}");
+}
